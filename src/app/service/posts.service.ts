deac99f6abb1032365cc4620c48805acb3a70589
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-11-28 14:58:54 +0100

    initial commit

---
+++ src/app/service/posts.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Post } from '../models/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  postArray: any;
+  constructor() { }
+
+  showPost(){
+    
+  }
+}
